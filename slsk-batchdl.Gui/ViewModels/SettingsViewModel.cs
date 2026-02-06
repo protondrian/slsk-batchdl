4974cf0bb0c3525aea6016e15e6929e4fbf0afdb
adrian <adrian.uhl@proton.me>
2026-02-07 00:23:39 +0100

    added crendential check

--- slsk-batchdl.Gui/ViewModels/SettingsViewModel.cs
+++ slsk-batchdl.Gui/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Soulseek;
 using slsk_batchdl.Gui.Models;
 using slsk_batchdl.Gui.Services;
 
@@ -26,6 +27,15 @@ public partial class SettingsViewModel : ObservableObject
     private string _downloadPath = Path.Combine(
         Environment.GetFolderPath(Environment.SpecialFolder.MyMusic), "sldl");
 
+    [ObservableProperty]
+    private string _credentialStatus = "";
+
+    [ObservableProperty]
+    private bool _credentialSuccess;
+
+    [ObservableProperty]
+    private bool _isTesting;
+
     public string[] AvailableFormats { get; } = ["MP3", "FLAC", "OGG", "M4A", "OPUS", "WAV"];
     public int[] AvailableBitrates { get; } = [128, 192, 256, 320];
 
@@ -45,6 +55,48 @@ public partial class SettingsViewModel : ObservableObject
         PreferredFormat = settings.PreferredFormat;
         PreferredBitrate = settings.PreferredBitrate;
         DownloadPath = settings.DownloadPath;
+    }
+
+    [RelayCommand]
+    private async Task TestCredentials()
+    {
+        if (!HasCredentials)
+        {
+            CredentialStatus = "Enter username and password first";
+            CredentialSuccess = false;
+            return;
+        }
+
+        IsTesting = true;
+        CredentialStatus = "";
+
+        try
+        {
+            using var client = new SoulseekClient();
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            await client.ConnectAsync(SoulseekUsername, SoulseekPassword, cts.Token);
+            CredentialStatus = "Login successful";
+            CredentialSuccess = true;
+        }
+        catch (SoulseekClientException ex)
+        {
+            CredentialStatus = ex.Message;
+            CredentialSuccess = false;
+        }
+        catch (TimeoutException)
+        {
+            CredentialStatus = "Connection timed out";
+            CredentialSuccess = false;
+        }
+        catch (Exception ex)
+        {
+            CredentialStatus = ex.Message;
+            CredentialSuccess = false;
+        }
+        finally
+        {
+            IsTesting = false;
+        }
     }
 
     [RelayCommand]
